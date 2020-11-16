f249bdbaba17feca29df962a77529e2165d8ba33
Jazmine <jazminejfox@gmail.com>
2020-11-16 17:05:58 -0600

    database setup

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,17 @@
 // TODO
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE drinkup", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
