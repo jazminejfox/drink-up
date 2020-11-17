e086217239c47966a58afc67e402fc813674b6d3
Jazmine <jazminejfox@gmail.com>
2020-11-17 12:38:44 -0600

    db saving works

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,43 @@
 // TODO
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection({
+// var con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: ''
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log('Connected!');
+//   con.query('CREATE DATABASE drinkup', function (err, result) {
+//     if (err) throw err;
+//     console.log('Database created');
+//   });
+// });
+
+const mysql = require('mysql');
+/*
+  - Create a database connection and export it from this file.
+  - You will need to connect with the user "root", no password,
+    and to the database "chat".
+*/
+
+let connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: ''
+  password: '',
+  database: 'drinkupdata',
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log('Connected!');
-  con.query('CREATE DATABASE drinkup', function (err, result) {
-    if (err) throw err;
-    console.log('Database created');
-  });
+let start = connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('connected');
 });
+
+module.exports = {
+  start,
+  connection
+};
